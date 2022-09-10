1c3a91f4196999606a6cdc61faff939413ed59b3
riolly <alberto.riolly@gmail.com>
2022-09-10 11:56:14 +0700

    feat: prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+	trailingComma: "es5",
+	useTabs: true,
+	tabWidth: 2,
+	semi: false,
+	singleQuote: true,
+	bracketSpacing: false,
+	jsxSingleQuote: true,
+	plugins: [require("prettier-plugin-tailwindcss")],
+	tailwindConfig: "./tailwind.config.cjs",
+};
