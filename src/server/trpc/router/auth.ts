49c32fe8b3ecbb8f8995c2805de527e5f55d2909
riolly <alberto.riolly@gmail.com>
2022-11-18 10:35:12 +0700

    :arrow_up: chore(deps): v6.10 update

--- src/server/trpc/router/auth.ts
+++ src/server/trpc/router/auth.ts
@@ -5,6 +5,6 @@ export const authRouter = router({
 		return ctx.session
 	}),
 	getSecretMessage: protectedProcedure.query(() => {
-		return 'You are logged in and can see this secret message!'
+		return 'You can see this secret message!'
 	}),
 })
