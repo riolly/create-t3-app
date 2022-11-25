b1e53f1918f7a20348a79304cf028cbc99635ac8
riolly <alberto.riolly@gmail.com>
2022-11-25 21:49:41 +0700

    :wrench: chore: allow google remote image

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,14 @@ const config = withBundleAnalyzer({
    * Reference repo for i18n:
    * @see https://github.com/juliusmarminge/t3-i18n
    **/
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+      },
+    ],
+  },
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
