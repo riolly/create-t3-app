c8856ebe835b82a2cccc9f01e90b87a84fc2d4d7
riolly <alberto.riolly@gmail.com>
2022-11-25 22:11:50 +0700

    :sparkles: chore: component layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,30 @@ import {trpc} from '@utils/trpc'
 
 import '@styles/globals.scss'
 
-import {type AppType} from 'next/app'
+import {type AppProps, type AppType} from 'next/app'
 import {type Session} from 'next-auth'
+import {type NextPage} from 'next/types'
+
+export type NextPageWithLayout<P = Record<string, unknown>, IP = P> = NextPage<
+	P,
+	IP
+> & {
+	getLayout?: (page: React.ReactElement) => React.ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+	Component: NextPageWithLayout
+}
 
 const MyApp: AppType<{session: Session | null}> = ({
 	Component,
 	pageProps: {session, ...pageProps},
-}) => {
+}: AppPropsWithLayout) => {
+	const getLayout = Component.getLayout || ((page) => page)
+
 	return (
 		<SessionProvider session={session}>
-			<Component {...pageProps} />
+			{getLayout(<Component {...pageProps} />)}
 		</SessionProvider>
 	)
 }
