4904cd4b88ed8d205a949e806ab4a96542586567
riolly <alberto.riolly@gmail.com>
2022-12-26 11:05:05 +0700

    :recycle: refactor: fine-grained classNames & infer label

--- src/components/textarea-input.tsx
+++ src/components/textarea-input.tsx
@@ -2,24 +2,36 @@ import {useFormContext} from 'react-hook-form'
 import {ErrorMessage} from '@hookform/error-message'
 import {capFirstChar} from 'utils/literal'
 
-import {type CreateArticleType} from 'types/article'
 type InputProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
-	name: keyof CreateArticleType
+	name: string
+	label?: string
+	wrapperClassName?: string
+	labelClassName?: string
+	inputClassName?: string
+	errorClassName?: string
 }
 
-const TextAreaInput = ({name, className, ...props}: InputProps) => {
+const TextAreaInput = ({
+	name,
+	label,
+	wrapperClassName,
+	labelClassName,
+	inputClassName,
+	errorClassName,
+	...props
+}: InputProps) => {
 	const {
 		register,
 		formState: {errors},
 	} = useFormContext()
 	return (
-		<div className='flex flex-col'>
-			<label htmlFor={name} className='text-light-primary'>
-				{capFirstChar(name)}
+		<div className={`flex flex-col ${wrapperClassName}`}>
+			<label htmlFor={name} className={`text-light-head ${labelClassName}`}>
+				{label ?? capFirstChar(name)}
 			</label>
 			<textarea
 				id={name}
-				className={`rounded bg-bg-light/80 py-2 px-4 ${className}`}
+				className={`bg-light-bg/80 rounded py-2 px-4 ${inputClassName}`}
 				{...register(name)}
 				{...props}
 			/>
@@ -27,7 +39,11 @@ const TextAreaInput = ({name, className, ...props}: InputProps) => {
 				name={name}
 				errors={errors}
 				render={({message}) => (
-					<small className='mt-0.5 font-medium text-red-500'>{message}</small>
+					<small
+						className={`mt-0.5 font-medium text-red-500 ${errorClassName}`}
+					>
+						{message}
+					</small>
 				)}
 			/>
 		</div>
